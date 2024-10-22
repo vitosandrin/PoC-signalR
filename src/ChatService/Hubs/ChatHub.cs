68ff1223d1fb831ed3f094fda93ad31d6f0e908f
vitosandrin <sandrinvito10@gmail.com>
2024-10-21 22:45:05 -0300

    Add project files.

---
+++ src/ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection connection)
+            => await Clients.All
+                    .SendAsync("ReceiveMessage", "admin", $"{connection.Username} has joined");
+
+        public async Task JoinSpecificChatRoom(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            await Clients.Group(connection.ChatRoom)
+                .SendAsync("ReceiveMessage", "admin", $"{connection.Username} has joined {connection.ChatRoom}");
+        }
+    }
+}
---
+++ src/ChatService/Program.cs
@@ -0,0 +1,26 @@
+using ChatService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
